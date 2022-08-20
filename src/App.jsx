ceaeac16470daa0c3f68933312cc0c31010690db
AndersonAD <alvesduarte97@gmail.com>
2022-08-20 20:00:42 -0300

    Working with display flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
+import "./App.css"
+
 import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
@@ -6,19 +8,25 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 
 export default () => (
-  <div id="app">
+  <div className="App">
     <h1>Fundamentos React</h1>
-    <Card titulo="#04 - Desafio Aleatório">
-      <Aleatorio min={5} max={100} />
-    </Card>
-    <Card titulo="#03 - Fragmento">
-      <Fragmento />
-    </Card>
-    <Card titulo="#02 - Com Parâmetro">
-      <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
-    </Card>
-    <Card titulo="#01 - Primeiro Componente">
-      <Primeiro />
-    </Card>
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={5} max={100} />
+      </Card>
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Pedro Silva"
+          nota={9.3}
+        />
+      </Card>
+      <Card titulo="#01 - Primeiro Componente">
+        <Primeiro />
+      </Card>
+    </div>
   </div>
 );
